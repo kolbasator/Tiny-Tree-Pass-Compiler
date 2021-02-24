291ae230c94a34e70d92cdc255c5a6e6134a8c07
Petro Kolosov <pkolosov@openskydata.com>
2021-02-24 22:07:47 +0100

    project updated and commented

---
+++ Compilier/Compiler/AstOperator.cs
@@ -0,0 +1,14 @@
+﻿namespace Compiler
+{
+    public class AstOperator : AbstractSyntaxTree
+    {
+        // you have already inherited all from AbstractSyntaxTree, here only constructor
+        public AstOperator(string value, AbstractSyntaxTree left, AbstractSyntaxTree right)
+        {
+            Value = value;
+            LeftChild = left;
+            RightChild = right;
+            Status = "Op";
+        }
+    }
+}
